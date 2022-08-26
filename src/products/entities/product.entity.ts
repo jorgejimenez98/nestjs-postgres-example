5e47c6191bf70eeb8afe3b7e281736b50ec62d7c
Jorge Jimenez <georgeguitarra07@gmail.com>
2022-08-26 17:14:15 +0200

    add tag column

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -35,6 +35,13 @@ export class Product {
   @Column('text')
   gender: string
 
+  @Column({
+    type: 'text',
+    array: true,
+    default: [],
+  })
+  tags: string[]
+
   @BeforeInsert()
   checkSlugInsert() {
     if (!this.slug) {
