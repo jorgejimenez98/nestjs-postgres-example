5e9dcfce3449b91db0304ea35ad4388f77775ab9
Jorge Jimenez <georgeguitarra07@gmail.com>
2022-08-26 14:18:29 +0200

    add global start url

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+  app.setGlobalPrefix('api') // Todo empieza por aqui
   await app.listen(3000)
 }
 bootstrap()
