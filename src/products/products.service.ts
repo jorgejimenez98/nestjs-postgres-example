908fc0629b3ab50f59a937e3ddf5454f9c04c121
Jorge Jimenez <georgeguitarra07@gmail.com>
2022-08-26 16:41:24 +0200

    add filter by params

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,16 +1,17 @@
 import {
-  BadRequestException,
-  Injectable,
-  InternalServerErrorException,
   Logger,
+  Injectable,
   NotFoundException,
+  BadRequestException,
+  InternalServerErrorException,
 } from '@nestjs/common'
-import { InjectRepository } from '@nestjs/typeorm'
 import { Repository } from 'typeorm'
+import { InjectRepository } from '@nestjs/typeorm'
+import { Product } from './entities/product.entity'
 import { CreateProductDto } from './dto/create-product.dto'
 import { UpdateProductDto } from './dto/update-product.dto'
-import { Product } from './entities/product.entity'
 import { PaginationDto } from '../common/dtos/pagination.dto'
+import { validate as isUUID } from 'uuid'
 
 @Injectable()
 export class ProductsService {
@@ -36,9 +37,14 @@ export class ProductsService {
     return this.productRepository.find({ take: limit, skip: offset })
   }
 
-  async findOne(id: string) {
-    const product = await this.productRepository.findOneBy({ id })
-    if (!product) throw new NotFoundException(`Product with id ${id} not found`)
+  async findOne(term: string): Promise<Product> {
+    let product: Product
+    if (isUUID(term)) {
+      product = await this.productRepository.findOneBy({ id: term })
+    } else {
+      product = await this.productRepository.findOneBy({ slug: term })
+    }
+    if (!product) throw new NotFoundException(`Product with ${term} not found`)
     return product
   }
 
